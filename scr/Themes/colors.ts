a613a06b3aa94e9bfddb3b3490c0a727f98e4815
bibhuti9 <bibhutiswain990@gmail.com>
2022-12-11 22:15:58 +0530

    first commit

---
+++ scr/Themes/colors.ts
@@ -0,0 +1,21 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('screen');
+export const colors = {
+  primary: '#4ed48d',
+  secondary: '#4e7bd4',
+  gray1: '#f0eeeb',
+  gray2: '#e3e1dc',
+  gray3: '#b5b4b1',
+};
+export const sizes = {
+  padding: 10,
+  margin: 10,
+  radious: 10,
+  height,
+  width,
+  XL: 30,
+  L: 27,
+  M: 25,
+  S: 20,
+  SM: 15,
+};
